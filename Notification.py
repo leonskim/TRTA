d5c1398052bb46b9239f7e13047080edf8576b41
Leon S. Kim <minusnine@gmail.com>
2014-08-07 22:40:12 +0900

    Visual notification (but I still need some brilliant notification messages)

---
+++ Notification.py
@@ -0,0 +1,40 @@
+from PySide import QtCore, QtGui, QtDeclarative
+
+# Basic environment
+APP_NOTIFICATION_QML_PATH = "./qml/Notification.qml"
+
+
+class Notification(QtDeclarative.QDeclarativeView):
+
+    sig_set_size = QtCore.Signal(int, int)
+    sig_set_text = QtCore.Signal(str)
+
+    def __init__(self, parent=None):
+        super(Notification, self).__init__(parent)
+        self.setSource(QtCore.QUrl.fromLocalFile(APP_NOTIFICATION_QML_PATH))
+        self.setResizeMode(QtDeclarative.QDeclarativeView.SizeRootObjectToView)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        self.setStyleSheet("background:transparent")
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint)
+        self.viewport().setAutoFillBackground(False)
+
+        # QML signal binding
+        self.root = self.rootObject()
+        self.sig_set_size.connect(self.root.setSize)
+        self.sig_set_text.connect(self.root.setText)
+
+        # Position
+        # 	Since it's impossible to know the screen size and position in QML(QtQuick1.1),
+        #	Python should change it for the notification area and let QML know the size.
+        self.move(0, 0)
+        screen = QtGui.QDesktopWidget().screenGeometry()
+        self.setFixedSize(screen.width(), (screen.height() / 5))
+        self.sig_set_size.emit(screen.width(), (screen.height() / 5))
+
+        # Show!
+        self.show()
+
+    def notify(self, notify_msg):
+        self.sig_set_text.emit(notify_msg)
+
+
